e55597c45dba2278bdc81513b5fbc1754d267c8f
Adarsha <vatraiadarsh@gmail.com>
2022-03-31 08:16:41 +1100

    added user register action

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+import {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAILURE,
+} from "../constants/userConstants";
+
+export const register =
+  (
+    title,
+    firstName,
+    lastName,
+    gender,
+    email,
+    date_of_birth,
+    mobile_number,
+    address,
+    password
+  ) =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: USER_REGISTER_REQUEST,
+      });
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/users/register",
+        {
+          title,
+          first_name: firstName,
+          last_name: lastName,
+          gender,
+          email,
+          date_of_birth,
+          mobile_number,
+          address,
+          password,
+        },
+        config
+      );
+
+      dispatch({
+          type:USER_REGISTER_SUCCESS,
+          payload:data
+      })
+
+      localStorage.setItem("userInfo",JSON.stringify(data));
+
+    } catch (error) {
+        dispatch({
+            type:USER_REGISTER_FAILURE,
+            payload:error.response.data
+        })
+    }
+  };
