2db0eb3ed04f8bd9afb5c43101db2615dd2ef52f
Adarsha <vatraiadarsh@gmail.com>
2022-03-31 08:21:55 +1100

    added userRegister Reducer

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,18 @@
+const {
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAILURE,
+} = require("../constants/userConstants");
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+    case USER_REGISTER_FAILURE:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
