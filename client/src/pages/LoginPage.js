c6ef235fbb3471e10279aa8151c27a4092ae8882
Adarsha <vatraiadarsh@gmail.com>
2022-03-31 00:49:05 +1100

    login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,34 +1,79 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-const Login = () => (
-  <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2'  textAlign='center'>
-        <Image src='/logo.png' /> Log-in to your account
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [disabled, setDisabled] = useState(false);
 
-          <Button color='blue' fluid size='large'>
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? &nbsp;<Link to={"/register"} >Sign Up</Link>
-      </Message>
-    </Grid.Column>
-  </Grid>
-)
+  useEffect(() => {
+    const { email, password } = formData;
+    const validateUser = email !== "" && password !== "";
+    validateUser ? setDisabled(false) : setDisabled(true);
+    console.log(validateUser);
+  }, [formData]);
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" textAlign="center">
+          <Image src="/logo.png" /> Log-in to your account
+        </Header>
+        <Form onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail address"
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              name="email"
+              value={formData.email}
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              name="password"
+              value={formData.password}
+            />
+
+            <Button disabled={disabled} color="blue" fluid size="large">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? &nbsp;<Link to={"/register"}>Sign Up</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default Login;
